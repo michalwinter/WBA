6abf800cde803def79a919b42b896a953850afe8
michalwinter <54064888+michalwinter@users.noreply.github.com>
2021-06-11 01:35:06 +0200

    Finish Final project.

---
+++ zaverecna-eshop/products.js
@@ -0,0 +1,182 @@
+let data = [
+    {
+        "title": "Dunk Low 58 Gulf",
+        "img_url": "http://cdn.shopify.com/s/files/1/2999/5106/products/True-to-Sole-Dunk-SB-Low-Club-58-Gulf-01.png?v=1618316274",
+        "desc": "",
+        "price": {
+            "Kč": 5599,
+            "€": 219
+        }
+    },
+
+    {
+        "title": "Dunk Low Garden",
+        "img_url": "https://cdn.shopify.com/s/files/1/2358/2817/products/Wethenew-Sneakers-France-Nike-Dunk-Low-Community-Garden-CZ9747-900-1.png?v=1599654723",
+        "desc": "",
+        "price": {
+            "Kč": 4999,
+            "€": 199
+        }
+    },
+
+    {
+        "title": "Dunk Low Chicago",
+        "img_url": "https://cdn.shopify.com/s/files/1/2999/5106/products/nike_sb_dunk_low_pro_chicago_01_1024x.png?v=1604340898",
+        "desc": "",
+        "price": {
+            "Kč": 10999,
+            "€": 499
+        }
+
+    },
+    {
+        "title": "Dunk Low Strangelove",
+        "img_url": "https://cdn.shopify.com/s/files/1/2358/2817/products/Wethenew-Sneakers-France-Nike-SB-Dunk-Low-StrangeLove-1_2000x.png",
+        "desc": "",
+        "price": {
+            "Kč": 69999,
+            "€": 2999
+        }
+
+    },
+    {
+        "title": "Dunk Low Be@rbrick",
+        "img_url": "https://cdn.shopify.com/s/files/1/2358/2817/products/Wethenew-Sneakers-France-Nike-SB-Dunk-Low-Grateful-Dead-Bears-Yellow-CJ5378-700-1_e1988aff-d24e-4f4e-8d43-d66f6b301b1b.png?v=1600703061",
+        "desc": "",
+        "price": {
+            "Kč": 23999,
+            "€": 959
+        }
+
+    },
+    {
+        "title": "Dunk Low Off-TRNK",
+        "img_url": "https://images.restocks.net/products/CU6015-100/nike-air-rubber-dunk-off-white-unc-1-1000.png",
+        "desc": "",
+        "price": {
+            "Kč": 7499,
+            "€": 289
+        }
+
+    },
+    {
+        "title": "Dunk Low UNC",
+        "img_url": "https://images.restocks.net/products/DD1391-102/nike-dunk-low-university-blue-unc-2021-1-1000.png",
+        "desc": "",
+        "price": {
+            "Kč": 6499,
+            "€": 249
+        }
+
+    },
+    {
+        "title": "Dunk Low Orange",
+        "img_url": "https://images.restocks.net/products/DD1503-800/nike-dunk-low-laser-orange-w-1-1000.png",
+        "desc": "",
+        "price": {
+            "Kč": 4999,
+            "€": 199
+        }
+
+    },
+    {
+        "title": "Dunk Low Curry",
+        "img_url": "https://images.restocks.net/products/DD1390-100/nike-dunk-low-medium-curry-1-1000.png",
+        "desc": "",
+        "price": {
+            "Kč": 6999,
+            "€": 269
+        }
+
+    },
+    {
+        "title": "Jordan 1 High Dior",
+        "img_url": "https://images.restocks.net/products/CN8607-002/air-jordan-1-retro-high-dior-1-400.png",
+        "desc": "",
+        "price": {
+            "Kč": 499999,
+            "€": 19999
+        }
+
+    },
+    {
+        "title": "Jordan 1 Youretarded",
+        "img_url": "https://images.restocks.net/products/555088-140/air-jordan-1-retro-high-og-obsidian-university-blue-555088-140-1-1000.png",
+        "desc": "",
+        "price": {
+            "Kč": 10999,
+            "€": 499
+        }
+
+    },
+    {
+        "title": "Jordan 1 High Panda",
+        "img_url": "https://images.restocks.net/products/CD0461-007/air-jordan-1-high-panda-102466-1-1000.png",
+        "desc": "",
+        "price": {
+            "Kč": 29999,
+            "€": 1099
+        }
+
+    },
+    {
+        "title": "Jordan 1 High J. Shell",
+        "img_url": "https://images.restocks.net/products/DC3481-900/air-jordan-1-retro-high-j-balvin-1-1000.png",
+        "desc": "",
+        "price": {
+            "Kč": 10999,
+            "€": 499
+        }
+
+    },
+    {
+        "title": "Jordan 1 High Mocha",
+        "img_url": "https://images.restocks.net/products/555088-105/air-jordan-1-retro-high-dark-mocha-1-1000.png",
+        "desc": "",
+        "price": {
+            "Kč": 10999,
+            "€": 499
+        }
+
+    },
+    {
+        "title": "Jordan 1 High Shadow 2.0",
+        "img_url": "https://images.restocks.net/products/555088-035/air-jordan-retro-high-shadow-2-0-1-1000.png",
+        "desc": "",
+        "price": {
+            "Kč": 6999,
+            "€": 269
+        }
+
+    },
+    {
+        "title": "Jordan 1 High Turbo Green",
+        "img_url": "https://images.restocks.net/products/575441-311/air-jordan-1-retro-high-turbo-green-gs-1-1000.png",
+        "desc": "",
+        "price": {
+            "Kč": 10999,
+            "€": 499
+        }
+
+    },
+    {
+        "title": "Jordan 1 Mikael Green",
+        "img_url": "https://images.restocks.net/products/DB4612-300/air-jordan-1-retro-high-lucky-green-w-1-1000.png",
+        "desc": "",
+        "price": {
+            "Kč": 6999,
+            "€": 269
+        }
+
+    },
+    {
+        "title": "Jordan 1 High Smoke Grey",
+        "img_url": "https://images.restocks.net/products/555088-126/air-jordan-1-retro-high-light-smoke-grey-1-400.png",
+        "desc": "",
+        "price": {
+            "Kč": 6999,
+            "€": 269
+        }
+
+    },
+]
